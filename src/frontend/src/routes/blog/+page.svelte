f291f81a0adbccdc912bbe2239f9af5ec392ac80
Lukas <lukaskarel1997@gmail.com>
2025-11-05 16:42:56 +0100

    first try mdx component

---
+++ src/frontend/src/routes/blog/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import Article1 from "$lib/blog/entries/article1.mdx";
+</script>
+
+<Article1 />
+
--- src/frontend/svelte.config.js
+++ src/frontend/svelte.config.js
@@ -1,13 +1,14 @@
+import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-static';
-
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://svelte.dev/docs/kit/integrations
   // for more information about preprocessors
-  preprocess: vitePreprocess(),
-
+  preprocess: [vitePreprocess(), mdsvex({
+    extensions: ['.mdx']
+  })],
   kit: {
     // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
     // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
@@ -19,7 +20,8 @@ const config = {
       precompress: false,
       strict: true
     })
-  }
+  },
+  extensions: ['.svelte', '.mdx']
 };
 
 export default config;
