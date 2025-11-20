da964e782086317c651faaa97c8b521bd5623ec4
Lukas <lukaskarel1997@gmail.com>
2025-11-20 20:29:54 +0100

    add comment for clarification

--- src/frontend/src/routes/+layout.server.ts
+++ src/frontend/src/routes/+layout.server.ts
@@ -1,2 +1,6 @@
 export const prerender = true;
+
+// required with static-adapter otherwise /route/index.html is not generated.
+// static-adapter would generate /route.html and this does not work
+// with hosting with caddy
 export const trailingSlash = 'always';
